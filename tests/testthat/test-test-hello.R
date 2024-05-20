efbfa7c159fb9de87773c6eda1ca498df700ba22
Austin Stitz <astitz@charlotte.edu>
2024-05-20 17:55:53 -0400

    Add tests and README

---
+++ tests/testthat/test-test-hello.R
@@ -0,0 +1,3 @@
+test_that("hello() returns \"Hello world!\"", {
+  expect_equal(hello(), "Hello world!")
+})
---
+++ tests/testthat/test-test-square.R
@@ -0,0 +1,4 @@
+test_that("square() squares a number", {
+  expect_equal(square(2), 4)
+  expect_equal(square(3), 9)
+})
