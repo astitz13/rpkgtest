288321f6f69a2e3a17829c432cba992b58cd2ac2
Austin Stitz <astitz@charlotte.edu>
2024-05-20 17:47:10 -0400

    Add roxygen2 documentation

--- R/hello.R
+++ R/hello.R
@@ -1,4 +1,10 @@
-
+#' Hello world!
+#'
+#' @return The string, "Hello world!"
+#' @export
+#'
+#' @examples
+#' hello()
 hello <- function() {
   "Hello world!"
 }
--- R/square.R
+++ R/square.R
@@ -1,3 +1,12 @@
+#' Square a Number
+#'
+#' @param x Number to square
+#'
+#' @return The given number to the second power
+#' @export
+#'
+#' @examples
+#' square(3)
 square <- function(x) {
   x ^ 2
 }
